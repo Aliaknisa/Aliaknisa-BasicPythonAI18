11295c20db8ae911104e19c4012c8f3a0fd46336
unknown <aliaknisa11@gmail.com>
2022-08-03 13:43:32 +0700

    ada perubahan pada file ini

--- hello.py
+++ hello.py
@@ -1,1 +1,37 @@
-print ("Hello World")
+print ("Hello World") # Hastag ini berfungsi untuk memberikan komentar (Komentar 1 baris). Short Cut Hastag ini adalah CTRL + ?
+print("\n") # Kode ini untuk spasi antar output
+print("==========================================")
+
+x = 5
+y = 10
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+
+print("\n")
+print("==========================================")
+#CASTING
+x = int(2.8) # y will be 2
+y = float("4.2") # w will be 4.2
+z = str(3.0) # z will be '3.0'
+print(x)
+print(y)
+print(z)
+
+print("\n")
+print("==========================================")
+
+# Quiz Meet 1
+x = "I love AI"
+y = 1945
+z = 71.80
+m = True
+n = 2*6/3
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(m))
+print(type(n))
